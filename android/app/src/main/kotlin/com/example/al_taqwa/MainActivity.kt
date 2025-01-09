a6ff05a7efbe42b1cc79ea99d9716ec45d1636b7
shafie-isak <shafiewisak@gmail.com>
2025-01-09 17:55:21 +0300

    initail commit

---
+++ android/app/src/main/kotlin/com/example/al_taqwa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.al_taqwa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:al_taqwa/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AlTaqwa(),
+    )
+  );
+}
+
+
+class AlTaqwa extends StatefulWidget {
+  const AlTaqwa({super.key});
+
+  @override
+  State<AlTaqwa> createState() => _AlTaqwaState();
+}
+
+class _AlTaqwaState extends State<AlTaqwa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        title: const Text("Al Taqwa", style: TextStyle(color: AppColors.lightBlue),),
+        leading: const Icon(Icons.filter_list_rounded, color: AppColors.secondaryColor,),
+      ),
+    );
+  }
+}
